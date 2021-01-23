578fcac966b4475b95c5d43ccd25619954e41954
Eugene L <orbitalsqwib@gmail.com>
2021-01-24 04:31:40 +0800

    removed useless TODO

--- Kasei/Kasei/Delegates/SceneDelegate.swift
+++ Kasei/Kasei/Delegates/SceneDelegate.swift
@@ -25,7 +25,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
-        //TODO: Check auth details and decide on course of action
         if !authHandler.loggedIn() {
             presentLoginScreen()
         }
